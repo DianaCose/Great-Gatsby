50f612c6e60258efe40a08819f82eb1eed65e454
DianaCose <diana.cosebiuc@gmail.com>
2024-02-28 16:43:10 +0200

    first commit

---
+++ my-gatsby-site/gatsby-config.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [ {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "GA-TRACKING_ID", 
+        "AW-CONVERSION_ID", 
+        "DC-FLOODIGHT_ID", 
+      ]
+    }
+}]}
